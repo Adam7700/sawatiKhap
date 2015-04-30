4821ae52d02cc964e9d52b3795f5574145738032
Ben Duggan <benaduggan@gmail.com>
2015-04-30 13:25:09 -0400

    I figured out a lot of stuff! and I found a really easy approach to doing all the moving

--- ourGrammarListeners.java
+++ ourGrammarListeners.java
@@ -1,8 +1,11 @@
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.antlr.v4.runtime.*;
+
+import static java.awt.GraphicsEnvironment.*;
 
 public class ourGrammarListeners extends ourGrammarBaseListener {
     ourGrammarParser parser;
@@ -10,7 +13,9 @@ public class ourGrammarListeners extends ourGrammarBaseListener {
 	File fout = new File("sawat.sh");
 	BufferedWriter gramgram;
 	FileOutputStream fos;
-	
+    public int height, width;
+    public ArrayList<Integer> offsets = new ArrayList<Integer>();
+    private Rectangle boundaries = getLocalGraphicsEnvironment().getMaximumWindowBounds();
 
     public ourGrammarListeners(ourGrammarParser parser){
     	this.parser = parser;
@@ -19,6 +24,20 @@ public class ourGrammarListeners extends ourGrammarBaseListener {
 			gramgram = new BufferedWriter(new OutputStreamWriter(fos));
 		} catch (FileNotFoundException e) {
 		}
+        getWorkspaceBoundaries();
+    }
+
+    private void getWorkspaceBoundaries() {
+        int screenWidth = (int) boundaries.getMaxX();
+        int screenHeight = (int) boundaries.getMaxY();
+        height = (int) boundaries.getHeight();
+        width = (int) boundaries.getWidth();
+        System.out.println(height);
+        System.out.println(width);
+        System.out.println(boundaries.getCenterX());
+        System.out.println(boundaries.getCenterY());
+
+
     }
 
     @Override
@@ -26,7 +45,7 @@ public class ourGrammarListeners extends ourGrammarBaseListener {
     	// switched wmctrl to a different workspace
         	//System.out.print(ctx.children.get(0));
         	try {
-    			gramgram.write("wmctrl -s " + ctx.children.get(1)+"\n" );
+    			gramgram.write("\nworkspace " + ctx.children.get(1)+"\n" );
     			//gramgram.close();
     		} catch (IOException e) {
     			e.printStackTrace();
@@ -46,14 +65,27 @@ public class ourGrammarListeners extends ourGrammarBaseListener {
     }
  
     @Override public void enterAppoption(ourGrammarParser.AppoptionContext ctx) {
-    	if(ctx.children.get(0).toString().equals("args")){
-    		options.add(0," "+ctx.children.get(2).getText());
-    	}else{
-    		options.add("\n"+ctx.children.get(2).getText());
+    	if(ctx.children.get(0).toString().equals("args"))
+		{
+    		options.add(0, " " + ctx.children.get(2).getText() + " &");
     	}
+		else if(ctx.children.get(0).toString().equals("snap"))
+		{
+            String dimensions = ctx.children.get(2).getText();
+            if(dimensions.equals("fullscreen")){
+                // figure out what the ending should be
+                dimensions = "20,20,200,200 ";
+            }
+            options.add("\n" + "wmctrl -r :ACTIVE: -e 0," + dimensions);
+    	}
+		else
+		{
+            return;
+		}
     }
     
-    @Override public void exitApplication(ourGrammarParser.ApplicationContext ctx) { 
+    @Override public void exitApplication(ourGrammarParser.ApplicationContext ctx) {
+
     	for(String items:options){
     		try {
 				gramgram.write(items);
